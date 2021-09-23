eb3bb712449b25e746bc94e57f0c3fbd26ff1a9b
shi514652697 <ssnegi_4all@yahoo.com>
2021-09-23 12:30:52 -0500

    changes

--- src/main/java/com/learnkafka/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +29,21 @@ public class LibraryEventsController {
 		//libraryEventProducer.sendLibraryEventSynchronous(libraryEvent); //sync call
 		//libraryEventProducer.sendLibraryEvent_Approach2(libraryEvent);  // aproach 2
 		return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+		
+	}
+	
+	@PutMapping("/v1/libraryevent")
+	public ResponseEntity<?> putLibraryEvent(@RequestBody @Valid LibraryEvent libraryEvent) throws JsonProcessingException
+	{
+		if(libraryEvent.getLibraryEventId() == null)
+		{
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the LibraryId");
+		}
+		libraryEvent.setLibraryEventType(LibraryEventType.UPDATE);
+		libraryEventProducer.sendLibraryEvent(libraryEvent); // asyc call
+		//libraryEventProducer.sendLibraryEventSynchronous(libraryEvent); //sync call
+		//libraryEventProducer.sendLibraryEvent_Approach2(libraryEvent);  // aproach 2
+		return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
 		
 	}
 
